24f4d67ca802e7aa261a277bd0928b9c5c99d287
p huang <huang.pien@gmail.com>
2018-01-13 07:38:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function theBeatlesPlay(array1,array2){
+  var arrayMus = [];
+  for (let i=0; i<array1.length; i++) {
+  arrayMus.push (`${array1[i]} plays ${array2[i]}`)
+}
+  return arrayMus
+}
+
+
+function johnLennonFacts(facts){
+  
+  var excitedFacts=[]
+  
+  let i=0
+  
+  while (i<facts.length){
+  excitedFacts.push (`facts[i]!!!`)
+  i++}
+  
+  return excitedFacts
+}
+
+function iLoveTheBeatles(n){
+  var beatlesLove=[]
+  
+  do {
+    beatlesLove.push('I love the Beatles!')}
+  while (n<15);
+
+  return beatlesLove    
+}
